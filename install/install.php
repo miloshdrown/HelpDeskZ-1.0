79ce339e1697ada747bfe1e522b972830c7a6a39
Marvin Blakley <marvin.blakley@sten-tel.com>
2016-08-04 11:09:39 -0400

    enabled logging

--- install/install.php
+++ install/install.php
@@ -6,7 +6,9 @@
  * @author Evolution Script S.A.C.
  * @since 1.0.0
  */
-error_reporting(E_ALL & ~E_NOTICE);
+/*error_reporting(E_ALL & ~E_NOTICE);*/
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 session_start();
 require_once __DIR__.'/functions.php';
 require_once HELPDESKZ_PATH.'includes/classes/classMysql.php';
